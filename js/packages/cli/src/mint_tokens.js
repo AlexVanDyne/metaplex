a45b0bf90aec194600a9c9c31bdaed00d2ebfadb
AlexVanDyne <Alex.Van.Dyne@gmail.com>
2021-12-04 17:34:18 -0800

    updated with configs and assets

---
+++ js/packages/cli/src/mint_tokens.js
@@ -0,0 +1,5 @@
+CandyMachine = 'ts-node src\candy-machine-cli.ts'
+keypair = 'C:\Users\Alex\Documents\GitHub\metaplex\config\solana\dev_wallet_2.json'
+for (var i = 0; i < 4; i++){
+    CandyMachine mint_one_token 
+}
